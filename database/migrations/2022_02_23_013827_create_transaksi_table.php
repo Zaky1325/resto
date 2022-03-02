f49cff7d73ea817a31081e1199fcdd00037ef9bf
nabilahermawati <nblhrmwt@gmail.com>
2022-03-02 10:02:56 +0700

    first commit

---
+++ database/migrations/2022_02_23_013827_create_transaksi_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->varchar('nama_pelanggan');
+            $table->varchar('nama_menu');
+            $table->integer('jumlah');
+            $table->integer('total_harga');
+            $table->varchar('nama_pegawai');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
